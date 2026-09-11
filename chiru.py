3c0a50e1271e83935f7a18a6eafe543fca2732df
chiranjeevidasar777-beep <chiranjeevidasar777@gmail.com>
2026-09-11 12:25:58 +0530

    Replace bike list with student marks input program

--- chiru.py
+++ chiru.py
@@ -1,6 +1,23 @@
-bikes=["Giant", "Trek", "Cannondale"]
-x=bikes[-1]
-i=-1
-while i>=0:
-    print(bikes[i])
-    i-=1
+name=input("enter student name:")
+print("student:",name)
+english=int(input("enter english marks:"))
+python=int(input("enter python marks"))
+maths=int(input("enter maths marks"))
+science=int(input("enter science marks"))
+computer=int(input("enter computer marks"))
+total=english+python+maths+science+computer
+print("total marks:",total)
+percentage=total/500*100
+print("percentage:",percentage,"%")
+if percentage>=79:
+    print=("A")
+elif percentage>=70:
+    print=("B")
+elif percentage>=60:
+    print("C")
+elif percentage>=50:
+    print("D")
+
+else:
+    print("fail")
+    print("grade:",grade)
